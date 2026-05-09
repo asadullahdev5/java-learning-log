195047aec70e46d71b09dc45d5cb3df30001a44b
asadullahdev5 <asadullahdev5@gmail.com>
2026-05-09 21:54:58 +0500

    Making a Simple Calculator

---
+++ Simple_Calculator/calculator.java
@@ -0,0 +1,22 @@
+
+import java.util.*;
+
+public class calculator {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+// Input numbers
+        System.out.println("First number: ");
+        double num1 = input.nextDouble();
+
+        System.out.println("Second number: ");
+        double num2 = input.nextDouble();
+// Input operator
+        System.out.println("Enter Operator (+ , - , * , /):");
+        char op = input.next().charAt(0);
+
+        double result;
+// Conditions
+    }
+}
