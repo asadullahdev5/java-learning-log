6904293a64a896033c26fe210cf3489ccf33ee91
asadullahdev5 <asadullahdev5@gmail.com>
2026-05-10 07:37:46 +0500

    Added even number printer

---
+++ Print_Even/main.java
@@ -0,0 +1,12 @@
+public class main {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            if(i %2 ==0){
+                System.out.println(i);   
+            }else{
+                System.out.println(" ");
+            }
+            
+        }
+    }
+}
