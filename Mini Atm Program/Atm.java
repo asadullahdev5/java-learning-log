0f3718b217daaaff1ab51cc7f4624239aa1b3da5
asadullahdev5 <asadullahdev5@gmail.com>
2026-05-14 15:16:52 +0500

    Updated Atm

--- Mini Atm Program/Atm.java
+++ Mini Atm Program/Atm.java
@@ -1,28 +1,31 @@
+
 import java.util.*;
+
 class Atm {
+       float Balance;
+        int PIN = 5674;
+        Scanner input = new Scanner(System.in);
 
-    float Balance;
-    int PIN =  5674;
-    Scanner input = new Scanner(System.in);
+        public void checkPin() {
 
-    public void checkpin() {
-        
-        for (int i = 1; i<=3; i++) {
-            System.out.println("Enter Your Pin");
-            int enteredpin = input.nextInt();
-            if (enteredpin == PIN) {
-                menu();
-                break;
-            } else if (enteredpin != PIN) {
-                System.out.println("Invalid Pin");
-                System.out.println((3-i ) + "Chances Left");
-                
-            } else if (i == 3){
-                System.out.println("Your card is blocked");
+            for (int i = 1; i <= 3; i++) {
+                System.out.println("Enter Your Pin");
+                int enteredpin = input.nextInt();
+                if (enteredpin == PIN) {
+                    menu();
+                    break;
+                } else {
+                    System.out.println("Invalid Pin");
+                }
+                if (i == 3) {
+                    System.out.println("Your Card Is Blocked");
+                } else {
+                    System.out.println((3 - i) + " Chances Left");
+                }
             }
 
         }
-    }
+    
 
     public void menu() {
 
@@ -33,52 +36,50 @@ class Atm {
         System.out.println("3. Deposit Money");
         System.out.println("4. Exit");
 
-        
-
         int opt = input.nextInt();
 
         if (opt == 1) {
 
             checkBalance();
-           
 
         } else if (opt == 2) {
 
             WithdrawMoney();
-          
+
         } else if (opt == 3) {
 
             DepositMoney();
-           
 
         } else if (opt == 4) {
 
-            return ;
+            return;
 
         } else {
 
             System.out.println("Print A Valid Number Mother Fucker");
+            menu();
         }
 
     }
 
-    public void checkBalance(){
+    public void checkBalance() {
         System.out.println("Balance" + Balance);
         menu();
     }
-    public void WithdrawMoney(){
+
+    public void WithdrawMoney() {
         System.out.println("Enter Withdrawal Amount Here:");
         float Amount = input.nextFloat();
         if (Amount > Balance) {
-            System.out.println("Insufficent Fund");  
-        } 
-        else { Balance = Balance - Amount;
+            System.out.println("Insufficent Fund");
+        } else {
+            Balance = Balance - Amount;
             System.out.println("Balance" + Balance);
         }
         menu();
     }
 
-    public void DepositMoney(){
+    public void DepositMoney() {
         System.out.println("Enter Deposit Amount Here:");
         Float Amount = input.nextFloat();
         Balance = Amount + Balance;
@@ -88,12 +89,7 @@ class Atm {
 
     public static void main(String[] args) {
         Atm obj = new Atm();
-        obj1.
+        obj.checkPin();
+
     }
-} 
-
-
-
-
-
-
+}
