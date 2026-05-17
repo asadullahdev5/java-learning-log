ada0c0690b10a20a312e0924dffd0549e9821549
asadullahdev5 <asadullahdev5@gmail.com>
2026-05-17 06:02:03 +0500

    Updated  menu code in Car_ShowRoom_Management

--- Car_ShowRoom_Management/menu.java
+++ Car_ShowRoom_Management/menu.java
@@ -1,6 +1,9 @@
 
+import java.util.Scanner;
+
 public class menu {
-    static void main_menu(){
+
+    static void main_menu() {
         System.out.println();
         System.out.println("╔══════════════════════════════════════════════════════════════════════════════════════════════╗");
         System.out.println("║                 🚗 WELCOME TO SHOWROOM MANAGEMENT SYSTEM 🚗                                 ║");
@@ -16,9 +19,24 @@ public class menu {
         System.out.println("║                                                                                       ║");
         System.out.println("║                           ❌ ENTER 0 TO EXIT ❌                                      ║");
         System.out.println("╚═══════════════════════════════════════════════════════════════════════════════════════╝");
-    } 
+    }
 
     public static void main(String[] args) {
-        
+        Scanner input = new Scanner(System.in);
+        showroom Showroom[] = new showroom[5];
+        employees Employees[] = new employees[5];
+        car Car[] = new car[5];
+        int showroom_counter = 0;
+        int car_counter = 0;
+        int employee_counter = 0;
+        int choice = 100;
+        while (choice != 0){
+            
+            main_menu();
+            choice = input.nextInt();
+
+            while(choice !=9 && choice !=0);
+        }
+
     }
 }
