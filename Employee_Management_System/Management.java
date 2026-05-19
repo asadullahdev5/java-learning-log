1c5b046dec80530196f3628a67e29b75c0f35c7b
asadullahdev5 <asadullahdev5@gmail.com>
2026-05-20 01:35:00 +0500

    Working on Employee Management System

--- Employee_Management_System/Management.java
+++ Employee_Management_System/Management.java
@@ -3,134 +3,74 @@ import java.util.Scanner;
 // Employee Mangement
 // Parent Class
 
-class Employee {
+abstract  class Employee {
 
-    int id;
-    String name;
-    float sallery;
+    private int id;
+    private String name;
+    private float salary;
 
     // Constructor
-    public Employee(int id, String name, float sallery) {
+    public Employee(int id, String name, float salary) {
         this.id = id;
         this.name = name;
-        this.sallery = sallery;
+        this.salary = salary;
+    }
+    
+        public int getId(){
+            return id;
+        }
+    
+        public String getName(){
+            return name;
+        }
+    
+        public float  getSalary(){
+            return salary;
+        }
+    
+
+
+    public abstract float calculateSalary();
+
+    @Override
+    public String toString (){
+        return "Employee[name "+name+",id = "+id+",salary="+calculateSalary()+"]";
     }
 
-    //Display Method
-    void displayInfo() {
-        System.out.print("=================");
-        System.out.println("Employee ID" + id);
-        System.out.println("Employee Name" + name);
-        System.out.println("Employee Sallery" + sallery);
-
-    }
+    
 }
 // Child Class Manager
 
 class Manager extends Employee {
 
-    String department;
+    private String department;
 
     // Child Constructor
-    public Manager(String department) {
-        super(0, null, 0);
+    public Manager(int id,String name , float salary ,String department) {
+        super(id, name, salary);
         this.department = department;
     }
 
-    void displayInfo() {
-        System.out.print("Employee Department: " + department);
+    public String getDepartment(){
+        return department;
     }
+
 }
+
 // Child Class Developer
 
 class Developer extends Employee {
 
-    String programmingLanguage;
+    private String programmingLanguage;
 
     // Child Constructor
-    public Developer() {
-        super(0, null, 0);
+    public Developer(int id,String name , float salary ,String programmingLanguage) {
+        super(id, name, salary);
+        this.programmingLanguage = programmingLanguage;
     }
 
-}
-
-// Main class
-public class Management {
-
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        Employee[] employees = new Employee[10];
-
-        int count = 0;
-        // Console Menu
-        while (true) {
-            System.out.println("===========EMPLOYEE MANAGEMENT SYSTEM===============");
-            System.out.println("1) Add Manager");
-            System.out.println("2) Add Devloper");
-            System.out.println("3) Display All Employees");
-            System.out.println("4) EXIT");
-
-            System.out.println("ENTER CHOICE: ");
-            int choice = input.nextInt();
-
-            switch (choice) {
-                case 1:
-                    if (count < employees.length) {
-                        System.out.println("Employee ID: ");
-                        int id = input.nextInt();
-                        input.nextLine();
-
-                        System.out.println("Employee Name: ");
-                        String name = input.nextLine();
-
-                        System.out.println("Employee Department: ");
-                        String department = input.nextLine();
-
-                        employees[count] = new Employee(id, name, department);
-                        count++;
-
-                        System.out.println("Added Employee SuccessFully");
-
-                    } else {
-                        System.out.println("Storage Full");
-                    }
-                    break;
-
-                case 2:
-
-                    for (int i = 0; i < count; i++) {
-                        books[i].display();
-                    }
-                    break;
-
-                case 3:
-                    System.out.println("Enter Book ID to Search: ");
-                    int SearchID = input.nextInt();
-                    boolean found = false;
-                    for (int i = 0; i < count; i++) {
-                        if (books[i].getBookId() == SearchID) {
-                            books[i].display();
-                            found = true;
-                            break;
-                        }
-                    }
-                    if (!found) {
-                        System.out.println("Book Not Found");
-                    }
-                    break;
-
-                case 4:
-
-                    System.out.println("Exiting...");
-
-                    System.exit(0);
-
-                default:
-
-                    System.out.println("Invalid Choice");
-
-            }
-        }
-
+    public String getprogrammingLanguage(){
+        return programmingLanguage;
     }
+
 }
